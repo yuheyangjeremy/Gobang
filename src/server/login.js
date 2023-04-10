195f5faa3979615043799c09264831d56562fe4e
yuheyangjeremy <79984472+yuheyangjeremy@users.noreply.github.com>
2023-04-10 16:31:16 +0800

    signup

--- src/server/login.js
+++ src/server/login.js
@@ -15,6 +15,7 @@ db.once("open", function () {
         res.send('Please login');
     });
 
+    // login here
     router.post('/login_check', (req, res) => {
 
         username = req.body['username'];
@@ -32,26 +33,63 @@ db.once("open", function () {
                 else {
                     if (result == null) {
                         res.set('Content-Type', 'application/json');
-                        res.send({ text: "Invalid username or password." });
+                        res.send({ validity: false, text: "Invalid username or password." });
                     } else {
                         //successfully login
                         if (result.admin) {
                             res.set('Content-Type', 'application/json');
                             temp_string = "Welcome administrator " + result.username + "!";
-                            data = { isAdmin: 1, text: temp_string };
+                            data = { validity: true, isAdmin: true, username: result.username, text: temp_string };
                             res.json(data);
                         }
                         else {
                             res.set('Content-Type', 'application/json');
                             temp_string = "Welcome " + result.username + "!";
                             result.text = temp_string;
-                            data = { isAdmin: 0, text: temp_string };
+                            data = { validity: true, isAdmin: false, username: result.username, text: temp_string };
                             res.json(data);
                         }
                     }
                 }
             })
 
+    })
+
+    //sign up here
+    router.post('/sign_up', (req, res) => {
+        username = req.body['username'];
+        password = req.body['password'];
+
+        User.findOne({ username: username })
+            .exec((err, result) => {
+                console.log(err);
+                console.log(result);
+
+                if (err) {
+                    res.set('Content-Type', 'text/plain');
+                    res.send(err);
+                }
+                if (result != null){
+                    res.set('Content-Type', 'application/json');
+                    res.send({ validity: false, text: "User already exists, please change your username!" });
+                }else{
+                    User.create({ username: username, pwd: password, admin: false }, (err, result) => {
+                        console.log(err);
+                        console.log(result);
+        
+                        if (err) {
+                            res.set('Content-Type', 'text/plain');
+                            res.send(err);
+                        }else{
+                            res.set('Content-Type', 'application/json');
+                            temp_string = "Welcome " + result.username + "!";
+                            result.text = temp_string;
+                            data = { validity: true, isAdmin: false, username: result.username, text: temp_string };
+                            res.json(data);
+                        }
+                    })
+                }
+            })
     })
 })
 
