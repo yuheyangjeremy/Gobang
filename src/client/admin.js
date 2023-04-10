4223abf9b39320231ff631349d82458bc7a809b3
yuheyangjeremy <79984472+yuheyangjeremy@users.noreply.github.com>
2023-04-10 17:37:53 +0800

    admin added

--- src/client/admin.js
+++ src/client/admin.js
@@ -2,14 +2,56 @@ import ReactDOM from "react-dom/client";
 import React, { useRef, useEffect, useState } from 'react';
 import $ from 'jquery';
 
-class Admin extends React.Component{
-    render(){
-        return(
-            <div>
-                <h1>Welcome admin</h1>
-            </div>
-        )
-    }
-}
+function Admin() {
+    const [users, setUsers] = useState([]);
 
-export default Admin;
+    useEffect(() => {
+        // "localhost:3000" could be omitted by setting proxy in package.json
+        fetch("http://localhost:3000/admin/users", {method: "GET"})
+        .then((res) => res.json())
+        .then((data) => setUsers(data))
+        .catch((err) => console.error(err));
+    }, []);
+
+    const deleteUser = (uid) => {
+        // "localhost:3000" could be omitted by setting proxy in package.json
+        let delete_url = "http://localhost:3000/admin/users/" + uid;
+        fetch(delete_url, { method: "DELETE" })
+        .then(res => res.json())
+        .then(response =>{
+            console.log(response);
+            alert("Deleted user " + uid);
+        })
+
+        // update user data
+        setUsers(users.filter(user => user._id !== uid));
+    };
+  
+    return (
+        <div className="container">
+        <h1>Admin Page</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Username</th>
+              <th>Password</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => (
+              <tr key={user._id}>
+                <td>{user.username}</td>
+                <td>{user.pwd}</td>
+                <td>
+                  <button className="btn btn-danger" onClick={() => deleteUser(user._id)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  
+  export default Admin;
