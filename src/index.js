f0059f35d5ba934ba3f11b611ff0f84d37697257
yuheyangjeremy <79984472+yuheyangjeremy@users.noreply.github.com>
2023-04-10 16:30:18 +0800

    routers updated

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import React from 'react';
 
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
+import Home from './client/home';
 import Login from './client/login';
+import User from './client/user';
+import Admin from './client/admin';
 
 const APP = () => {
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/login" element={<Login />} />
+                <Route path="/index" element={<Home />} />
             </Routes>
         </BrowserRouter>
     );
